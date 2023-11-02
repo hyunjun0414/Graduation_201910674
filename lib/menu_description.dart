bae78a9f00e7a435b418b3008a5b408223a49239
HyunJunLee <guswns0414@naver.com>
2023-11-02 16:17:41 +0900

    feedback 페이지 구현예정

--- lib/menu_description.dart
+++ lib/menu_description.dart
@@ -17,6 +17,7 @@ class DisplayPictureScreenState extends State<DisplayPictureScreen> {
   String? extractedText;
   DocumentSnapshot? firestoreData;
   final bool hasAllergyInfo = true; // 알러지 정보가 있는지 여부를 나타내는 변수
+
   @override
   void initState() {
     super.initState();
@@ -38,6 +39,29 @@ class DisplayPictureScreenState extends State<DisplayPictureScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (firestoreData == null) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Color(0xffCDF5F9),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("일치하는 데이터가 없습니다. 피드백을 제공해주시면 감사하겠습니다!"),
+                SizedBox(height: 50,),
+                ElevatedButton(
+                  onPressed: _requestFeedback,
+                  child: Text("피드백 제공"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    // 일치하는 데이터가 있는 경우의 기존 UI 반환
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -74,9 +98,7 @@ class DisplayPictureScreenState extends State<DisplayPictureScreen> {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(
-                  height: 17,
-                ),
+                SizedBox(height: 17),
                 Text(
                   (firestoreData?.data() as Map<String, dynamic>?)?['description'] ?? 'MenuName',
                   textAlign: TextAlign.center,
@@ -91,5 +113,9 @@ class DisplayPictureScreenState extends State<DisplayPictureScreen> {
         ),
       ),
     );
+  }
+
+  void _requestFeedback() {
+    // 피드백을 받는 코드 (예: 알림창 또는 다른 페이지로 이동)
   }
 }
